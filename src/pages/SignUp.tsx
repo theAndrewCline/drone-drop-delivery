34b1e00af4a94e0f098bbc3ca70bfc3c7c9e6ad7
Andrew Cline <andrew.cline77@gmail.com>
2020-11-28 21:38:55 -0600

    feat: add address confirmation

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -5,9 +5,13 @@ import { Redirect } from 'react-router-dom'
 import { validateAddress, Address } from '../lib/address'
 import { useCreateUserMutation } from '../lib/user'
 import { SignUpForm } from '../components/SignUpForm'
+import Bounce from 'react-reveal/Bounce'
 
 export function SignUpPage() {
   const [shouldRedirect, setShouldRedirect] = useState(false)
+  const [potentialAddress, setPotentialAddress] = useState<Address | undefined>(
+    undefined
+  )
   const [error, setError] = useState(false)
 
   const [name, setName] = useState<string>('')
@@ -27,7 +31,17 @@ export function SignUpPage() {
 
   function handleFormSubmit() {
     validateAddress(address)
-      .then((a: Address) => createUser({ name, address: a }))
+      .then((a) => {
+        setPotentialAddress(a)
+      })
+      .catch((_) => {
+        setError(true)
+        setShouldRedirect(true)
+      })
+  }
+
+  function handleAddressConfirm() {
+    createUser({ name, address: (potentialAddress as unknown) as Address })
       .then(() => {
         setShouldRedirect(true)
       })
@@ -35,6 +49,47 @@ export function SignUpPage() {
         setError(true)
         setShouldRedirect(true)
       })
+  }
+
+  function handleAddressDeny() {
+    setPotentialAddress(undefined)
+  }
+
+  if (potentialAddress && !shouldRedirect) {
+    return (
+      <Page>
+        <Bounce up>
+          <div className="z-10 flex flex-col w-8/12 h-auto p-4 text-lg bg-gray-200 rounded-lg">
+            <h1 className="mb-2 font-bold">Is this your address?</h1>
+            <p>{potentialAddress.street}</p>
+            <p>{potentialAddress.secondary}</p>
+            <p>
+              {potentialAddress.city} {potentialAddress.state},{' '}
+              {potentialAddress.zipcode}
+            </p>
+            <div className="flex w-100%">
+              <button
+                className="flex-1 px-4 py-2 my-4 font-bold text-white bg-green-500 rounded transition duration-500 hover:bg-green-600 hover:shadow-xl"
+                onClick={() => handleAddressConfirm()}
+              >
+                Yes
+              </button>
+
+              <button
+                className="flex-1 px-4 py-2 my-4 ml-2 font-bold text-white bg-red-500 rounded transition duration-500 hover:bg-red-600 hover:shadow-xl"
+                onClick={() => handleAddressDeny()}
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </Bounce>
+
+        <div className="absolute">
+          <DroneDelivery />
+        </div>
+      </Page>
+    )
   }
 
   switch (shouldRedirect) {
@@ -47,7 +102,6 @@ export function SignUpPage() {
     case false:
       return (
         <Page>
-          <div className="flex flex-col items-center justify-center"></div>
           <SignUpForm
             handleFormSubmit={handleFormSubmit}
             setName={setName}
